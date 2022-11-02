0a80162f589af9fecf2278579b1b6c227b7600ad
taran2003 <taran2003@bk.ru>
2022-11-02 21:07:30 +0300

    request test

--- index.js
+++ index.js
@@ -3,9 +3,34 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('get request');
+});
+
+app.get('/user', (req, res) => {
+    res.send('user path get request');
+});
+
+app.options('/', (req, res) => {
+    res.send('options request');
+});
+
+app.head('/', (req, res) => {
+    res.header('key','value');
+});
+
+app.put('/', (req, res) => {
+    res.send('put request');
+});
+
+app.patch('/', (req, res) => {
+    res.send('patch request');
+});
+
+
+app.delete('/', (req, res) => {
+    res.send('delete request');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
